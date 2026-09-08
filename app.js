abaf131ba3dbf5458d69a0c0c3654506418a483b
Micheal A Ibrahim <mickyebira@gmail.com>
2026-09-08 07:24:04 +0200

    fix cors

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ connect(process.env.MONGODB_URI);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  origin: "*", // Allow all origins
+  origin: ["http://localhost:3000/"],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
      credentials: true
 }));
